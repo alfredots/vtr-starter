3dfd62acc472f571107750300d0c04729ef2731a
alfredo <alfredtito97@gmail.com>
2022-08-08 10:30:32 -0300

    feat: montado archetype

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">
+        Vite + <span className="text-sky-500">Tailwind</span>
+      </h1>
+    </div>
+  )
+}
+
+export default App
