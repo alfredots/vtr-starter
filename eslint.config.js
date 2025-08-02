d458b108fef506750584edf068dffd765482e1e4
alfredo tito <tito.alfred@hotmail.com>
2025-08-02 19:53:35 -0300

    feat: update eslint to 9 version

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+// eslint.config.js
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+
+  {
+    files: ['**/*.tsx', '**/*.ts'],
+    plugins: {
+      react
+    },
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true
+      }
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off'
+    },
+    ignores: ['node_modules/**']
+  },
+  prettier
+];
