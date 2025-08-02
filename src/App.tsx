dbeeafea46b9b7221578d1a9f22ebf42be2ee6ee
alfredo tito <tito.alfred@hotmail.com>
2025-08-02 19:34:52 -0300

    feat: update packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { AppRoutes } from './routes'
-
 function App() {
-  return <AppRoutes />
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
-export default App
+export default App;
